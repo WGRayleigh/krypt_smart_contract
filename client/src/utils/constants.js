6adcad9c6255d4e716a177f03a6051fbb1d754c6
Guangrui Wang <wguangrui20@vt.edu>
2022-02-28 18:48:11 -0500

    finished smart contract transaction

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractAddress = "0x0cc79Cc34cfa0Aa50FFcbCE449306C6340F7D22D";
 export const contractABI = abi.abi;
